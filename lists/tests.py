de8435796e6a80683b69d716e723f91bb1930dad
Panachai Vankwan <katooo241@gmail.com>
2020-03-25 00:56:44 +0700

    Unit test for saving first term data

--- lists/tests.py
+++ lists/tests.py
@@ -41,3 +41,14 @@ class SignUpTest(TestCase):
 
         example_users = User.objects.all()
         self.assertEqual(example_users.count(), 4)
+
+    class GradeCalTest(TestCase):
+        def test_gradeCal_can_save_first_term_data(self):
+            example_user = Userinfo.objects.create(name='example_user ')
+            example_data = Term1.objects.create(subject='example_subject', unit='1', Grade='A')
+            example_user.term1.add(example_data)
+            user = Userinfo.objects.all()
+            example_saved_user = user[0]
+            self.assertEqual(example_saved_user.term1.first().subject, 'example_subject')
+            self.assertEqual(example_saved_user.term1.first().unit, '1')
+            self.assertEqual(example_saved_user.term1.first().Grade, 'A')
