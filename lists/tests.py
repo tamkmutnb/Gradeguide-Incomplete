5bd1c7d04bdccdd0241ee7e551ab50c16681bf99
Panachai Vankwan <katooo241@gmail.com>
2020-03-25 00:55:38 +0700

    Unit test for sign many users

--- lists/tests.py
+++ lists/tests.py
@@ -24,3 +24,20 @@ class SignUpTest(TestCase):
 
         example_users = User.objects.all()
         self.assertEqual(example_users.count(), 1)
+
+    def test_many_users_signup(self):
+        self.example_user1 = User.objects.create_user(username='Panachai', password='Panachaipasswordisveryeasy',
+                                                      email='panachai@test.com')
+        self.example_user1.save()
+        self.example_user2 = User.objects.create_user(username='Kristamet', password='Kristametpasswordisveryeasy',
+                                                      email='Kristamet@test.com')
+        self.example_user2.save()
+        self.example_user3 = User.objects.create_user(username='Nutnicha', password='Nutnichapasswordisveryeasy',
+                                                      email='Nutnicha@test.com')
+        self.example_user3.save()
+        self.example_user4 = User.objects.create_user(username='Watsawat', password='Watsawatpasswordisveryeasy',
+                                                      email='Watsawat@test.com')
+        self.example_user4.save()
+
+        example_users = User.objects.all()
+        self.assertEqual(example_users.count(), 4)
