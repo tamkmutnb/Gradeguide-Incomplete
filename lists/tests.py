9a0494a773ce631eb289d938c28a10469bb53e53
s6101012620148 <s6101012620148@email.kmutnb.ac.th>
2020-04-15 01:06:35 +0700

    comment tests.py
    comment all unit test

--- lists/tests.py
+++ lists/tests.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from lists.models import Userinfo,Term1,Term2,Term3,Term4,Term5,Term6,Term7,Term8,GPA
+from lists.models import Userinfo, Term1, Term2, Term3, Term4, Term5, Term6, Term7, Term8, GPA
+
 
 class HomePageTest(TestCase):
 
+    # test home page template
+    # go to homepage
+    # check if if response correct
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'base.html')
 
+
 class SignUpTest(TestCase):
+
+    # test home singup
+    # create user
+    # save user
+    # collect all objects into var
+    # check if var count = 1
     def test_user_signup(self):
         self.example_user = User.objects.create_user(username='Panachai', password='mypasswordisveryeasy',
                                                      email='panachai@test.com')
@@ -16,6 +27,18 @@ class SignUpTest(TestCase):
 
         example_users = User.objects.all()
         self.assertEqual(example_users.count(), 1)
+
+    # test sign up many users
+    # create user 1
+    # save user 1
+    # create user 2
+    # save user 2
+    # create user 3
+    # save user 3
+    # create user 4
+    # save user 4
+    # collect user.all to var
+    # check if user.all = 4
 
     def test_many_users_signup(self):
         self.example_user1 = User.objects.create_user(username='Panachai', password='Panachaipasswordisveryeasy',
@@ -34,23 +57,39 @@ class SignUpTest(TestCase):
         example_users = User.objects.all()
         self.assertEqual(example_users.count(), 4)
 
+
 class LogInTest(TestCase):
+    # create user
+    # login user
     def test_many_users_signup(self):
         User.objects.create_user(username='Panachai', password='Panachaipasswordisveryeasy')
         self.client.login(username="Panachai", password="Panachaipasswordisveryeasy")
 
+
 class GradeCalTest(TestCase):
     def test_gradeCal_can_save_first_term_data(self):
+        # create object example_user
         example_user = Userinfo.objects.create(name='example_user ')
+
+        # create object example_subject, unit and grade
         example_data = Term1.objects.create(subject='example_subject', unit='1', Grade='A')
+
+        # add example_data to user.term1
         example_user.term1.add(example_data)
+        # collect user.all to user var
         user = Userinfo.objects.all()
+        # save user[0] to example_saved_user var
         example_saved_user = user[0]
+        # check if example_saved_user.term1.first().subject = 'example_subject'
+        # check if example_saved_user.term1.first().unit = '1'
+        # check if example_saved_user.term1.first().Grade = 'A'
         self.assertEqual(example_saved_user.term1.first().subject, 'example_subject')
         self.assertEqual(example_saved_user.term1.first().unit, '1')
         self.assertEqual(example_saved_user.term1.first().Grade, 'A')
 
     def test_gradeCal_can_save_all_term_data(self):
+        # create user.object name example_user
+        # create user.objects data for each term
         example_user = Userinfo.objects.create(name='example_user ')
         example_data1 = Term1.objects.create(subject='example_subject1', unit='1', Grade='A')
         example_data2 = Term2.objects.create(subject='example_subject2', unit='2', Grade='B+')
@@ -61,6 +100,7 @@ class GradeCalTest(TestCase):
         example_data7 = Term7.objects.create(subject='example_subject7', unit='7', Grade='D+')
         example_data8 = Term8.objects.create(subject='example_subject8', unit='8', Grade='F')
 
+        # add each user.objects to their terms
         example_user.term1.add(example_data1)
         example_user.term2.add(example_data2)
         example_user.term3.add(example_data3)
@@ -70,9 +110,12 @@ class GradeCalTest(TestCase):
         example_user.term7.add(example_data7)
         example_user.term8.add(example_data8)
 
+        # collect objects.all to user var
+        # make example_saved_user var = user[0]
         user = Userinfo.objects.all()
         example_saved_user = user[0]
 
+        # check if each term = to their example_subject
         self.assertEqual(example_saved_user.term1.first().subject, 'example_subject1')
         self.assertEqual(example_saved_user.term2.first().subject, 'example_subject2')
         self.assertEqual(example_saved_user.term3.first().subject, 'example_subject3')
@@ -83,11 +126,18 @@ class GradeCalTest(TestCase):
         self.assertEqual(example_saved_user.term8.first().subject, 'example_subject8')
 
     def test_gradeCal_can_save_GPA(self):
+        # create example_user var
+        # create example GPA data
         example_user = Userinfo.objects.create(name='example_user ')
-        example_GPA_data = GPA.objects.create(GPA_1 = "4",GPA_2 = "3.5",GPA_3 = "3",GPA_4 = "2.5",GPA_5 = "2",GPA_6 = "1.5",GPA_7 = "1",GPA_8 = "0.5")
+        example_GPA_data = GPA.objects.create(GPA_1="4", GPA_2="3.5", GPA_3="3", GPA_4="2.5", GPA_5="2", GPA_6="1.5",
+                                              GPA_7="1", GPA_8="0.5")
+        # add example GPA to example_user.gpa
         example_user.gpa.add(example_GPA_data)
+        # collect objects.all to user
         user = Userinfo.objects.all()
+        # make example_saved_user = user[0]
         example_saved_user = user[0]
+        # check if each gpa objects is = their gpa
         self.assertEqual(example_saved_user.gpa.first().GPA_1, '4')
         self.assertEqual(example_saved_user.gpa.first().GPA_2, '3.5')
         self.assertEqual(example_saved_user.gpa.first().GPA_3, '3')
